6d50416cbd60285b709c9ff9669bc0e35d5a8627
Ian Alves <132668846+Alvesds-Ian@users.noreply.github.com>
2023-07-06 13:01:20 +0000

    fa

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ let lista = document.querySelector('select#flista')
 let res = document.querySelector('div#res')
 let valores = []
 
-function isNumero(n){
+function isNumero(n) {
     if(Number(n)>=1 && Number(n)<=100 ) {
         return true
     } else {
@@ -11,15 +11,16 @@ function isNumero(n){
     }
 }
 
-function inLista(n, l){
-    if(l.indexOf(Number(n) != -1))
+function inLista(n, l) {
+    if(l.indexOf(Number(n) != -1)) {
         return true
-    else
+    }else {
         return false 
+    }
 }
 
 function adicionar() {
-    if(isNumero(num.value) && inLista(num.value, valores)){
+    if(isNumero(num.value) && !inLista(num.value, valores)){
         valores.push(Number(num.value))
         let item = document.createElement('option')
         item.text = `Valor ${num.value} adicionado`
